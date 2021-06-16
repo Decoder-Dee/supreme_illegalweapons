6a0328ed8eb7cb7ec838b1781d5d8bc0f1cb2c64
Decoder-Dee <59232670+Decoder-Dee@users.noreply.github.com>
2021-06-16 20:04:38 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.ArgentSale = false  --true if use of black money, false for regular money
+
+Config.Weapons = {
+    {weapon = "WEAPON_CROWBAR", price = 115},
+    {weapon = "WEAPON_DAGGER", price = 200},
+    {weapon = "WEAPON_MOLOTOV", price = 2750},    
+    {weapon = "WEAPON_PISTOL50", price = 12800},
+    {weapon = "WEAPON_DOUBLEACTION", price = 16200},
+    {weapon = "WEAPON_SAWNOFFSHOTGUN", price = 22400},
+    {weapon = "WEAPON_MINISMG", price = 24800},
+    {weapon = "WEAPON_GUSENBERG", price = 42500}, 
+}
+
+Config.Items = {
+    {weapon = "water", price = 35},
+}
+
+--Weapon List : https://wiki.rage.mp/index.php?title=Weapons
+
+----------------------------------------
+--Dev by Thom512#0990 for Patoche#4702--
+----------------------------------------
